6b7057264d86d8aa03c9bddf441e46004dfc32c7
Peter Beattie <peterbeattie19@gmail.com>
2018-10-02 16:32:07 +0100

    You have to activate it

---
+++ exercise6.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define endl "\n"
+#define rep(i,a,b) for(int i = a; i<b; i++)
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int i,j,k;
+
+    int length = 5; 
+
+    cout << "***\n* *\n***\n\n"; 
+
+    cout << "***\n* *\n* *\n* *\n***\n\n";
+
+    cout << "   *\n  **\n * *\n****";
+    
+    return 0;
+}
---
+++ exercise7.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define endl "\n"
+#define rep(i,a,b) for(int i = a; i<b; i++)
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int i,j,k;
+    
+    rep(i, 0, 10){
+        if(i%2 == 0)
+            cout << i << endl; 
+    }    
+    return 0;
+}
